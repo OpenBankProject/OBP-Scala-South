9ce674941da0805f4b4a2be52cfcbfa94f6dd200
slavisa <slavisa.cicovic@md-softwaredesign.de>
2017-06-07 16:54:13 +0200

    code style, robustness

--- src/main/scala/com/tesobe/obp/ProcessorFactory.scala
+++ src/main/scala/com/tesobe/obp/ProcessorFactory.scala
@@ -1,7 +1,6 @@
 package com.tesobe.obp
 
-import com.tesobe.obp.Main.{executionContext, materializer, processorName}
-import com.tesobe.obp.SouthKafkaStreamsActor.Topic
+import com.tesobe.obp.SouthKafkaStreamsActor.{Topic, TopicBusiness}
 
 /**
   * Created by slavisa on 6/6/17.
@@ -10,9 +9,9 @@ object ProcessorFactory extends KafkaConfig {
 
   def getProcessor = {
     processorName match {
-      case "circe_Nov2016" => (Topic("Request", "Response"), CirceProcessor(RequestExtractor_Nov2016)(Main.executionContext, Main.materializer).processor)
-      case "json_Nov2016" => (Topic("Request", "Response"), Processor_Nov2016(RequestExtractor_Nov2016)(Main.executionContext).processor)
-      case _ => (Topic("Request", "Response"), CirceProcessor(RequestExtractor_Nov2016)(Main.executionContext, Main.materializer).processor)
+      case "circe_Nov2016" => TopicBusiness(Topic("Request", "Response"), CirceProcessor(RequestExtractor_Nov2016)(Main.executionContext, Main.materializer).processor)
+      case "json_Nov2016" => TopicBusiness(Topic("Request", "Response"), Processor_Nov2016(RequestExtractor_Nov2016)(Main.executionContext).processor)
+      case _ => TopicBusiness(Topic("Request", "Response"), CirceProcessor(RequestExtractor_Nov2016)(Main.executionContext, Main.materializer).processor)
     }
   }
 }
